d90327913168485a67920223a49f13e99494c048
Vitaly Cherenko <xpoh697@gmail.com>
2026-03-21 08:01:11 +0100

    Cleanup: Removed diagnostic attributes after successful verification

--- custom_components/energy_management/strategy.py
+++ custom_components/energy_management/strategy.py
@@ -1598,8 +1598,6 @@ class StrategyEngine:
                         cur_mode_text = "Ожидание (Пик цены)"
             
             res["current_mode_text"] = cur_mode_text
-            res["debug_charge_reason"] = str(res.get("charge_reason", "missing"))
-            res["debug_state"] = str(state)
             
             self._strategy_cache[cache_key] = {"time": now, "res": res}
             return res
