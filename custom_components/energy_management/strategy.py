24554569f74e5246044386ff20461e094895037e
Vitaly Cherenko <xpoh697@gmail.com>
2026-05-01 13:57:33 +0300

    v11.6.530: Fixed power sync and amps calculation

--- custom_components/energy_management/strategy.py
+++ custom_components/energy_management/strategy.py
@@ -1,5 +1,4 @@
 import logging
-# MarketStrategy Version: v11.6.530
 _LOGGER = logging.getLogger(__name__)
 from datetime import datetime, timedelta
 from typing import TYPE_CHECKING, Any, Dict, List, Tuple, Optional
