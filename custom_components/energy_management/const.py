e2f626569fca5c0ac10e41aff729a0e5c988e0af
Vitaly <vitaly@xpoh.ru>
2026-05-11 19:46:26 +0200

    v11.9.392: Synced modal dropdown labels with concise mode_labels

--- custom_components/energy_management/const.py
+++ custom_components/energy_management/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "energy_management"
-VERSION = "v11.9.389"
-VERSION_CODE = 1109389
+VERSION = "v11.9.392"
+VERSION_CODE = 1109392
 
 CONF_CONSUMPTION_SENSORS = "consumption_sensors"
 CONF_GENERATION_SENSORS = "generation_sensors"
