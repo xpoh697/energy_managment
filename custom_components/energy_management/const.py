0f503abc35c92bce33c6812fbed216dc211832d5
Vitaly <vitaly@xpoh.ru>
2026-05-11 19:51:49 +0200

    v11.9.397: Changed Stop Sale color to yellow/amber for better observability

--- custom_components/energy_management/const.py
+++ custom_components/energy_management/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "energy_management"
-VERSION = "v11.9.396"
-VERSION_CODE = 1109396
+VERSION = "v11.9.397"
+VERSION_CODE = 1109397
 
 CONF_CONSUMPTION_SENSORS = "consumption_sensors"
 CONF_GENERATION_SENSORS = "generation_sensors"
