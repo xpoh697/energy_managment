2a7d1b14b03f73a1d2392b295865fe5a44102753
Vitaly <vitaly@xpoh.ru>
2026-05-11 14:15:00 +0200

    v11.9.360: Centered layout with stats grid below gauge

--- custom_components/energy_management/const.py
+++ custom_components/energy_management/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "energy_management"
-VERSION = "v11.9.359"
-VERSION_CODE = 1109359
+VERSION = "v11.9.360"
+VERSION_CODE = 1109360
 
 CONF_CONSUMPTION_SENSORS = "consumption_sensors"
 CONF_GENERATION_SENSORS = "generation_sensors"
