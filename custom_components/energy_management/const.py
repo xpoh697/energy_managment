a77c5200a267fc5a20a0ff6954d90591a59ad4ed
Vitaly <vitaly@xpoh.ru>
2026-05-12 10:20:48 +0200

    v11.9.425: Implemented survival trigger as min_soc + 0.5 * buffer

--- custom_components/energy_management/const.py
+++ custom_components/energy_management/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "energy_management"
-VERSION = "v11.9.423"
-VERSION_CODE = 1109423
+VERSION = "v11.9.425"
+VERSION_CODE = 1109425
 
 CONF_CONSUMPTION_SENSORS = "consumption_sensors"
 CONF_GENERATION_SENSORS = "generation_sensors"
