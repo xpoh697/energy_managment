d1da2b2f1e74b201fbd4ea197e36a4b03b7e217e
Vitaly <vitaly@xpoh.ru>
2026-05-13 00:49:54 +0200

    v11.9.499: Emergency fix for NameError sim_log

--- custom_components/energy_management/const.py
+++ custom_components/energy_management/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "energy_management"
-VERSION = "v11.9.498"
-VERSION_CODE = 1109498
+VERSION = "v11.9.499"
+VERSION_CODE = 1109499
 
 CONF_CONSUMPTION_SENSORS = "consumption_sensors"
 CONF_GENERATION_SENSORS = "generation_sensors"
--- custom_components/energy_management/strategy_buy.py
+++ custom_components/energy_management/strategy_buy.py
@@ -73,7 +73,7 @@ class StrategyBuy(StrategyEngine):
         prof_thresh = float(man.get_setting(CONF_ARBITRAGE_PROFIT_THRESHOLD, 0.5))
 
         res = {
-            "strategy_version": "v11.9.494",
+            "strategy_version": "v11.9.499",
             "state": "standard",
             "mode": mode,
             "active_hours": [],
@@ -368,6 +368,9 @@ class StrategyBuy(StrategyEngine):
             if charge_commands:
                 _LOGGER.warning(f"[Strategy Buy] FINAL Simulation Keys: {list(charge_commands.keys())} | Vals: {list(charge_commands.values())}")
             
+            # 3. Final Simulation to get REAL progressive SOC levels (Chronological)
+            soc_end, sim_log, _ = self.run_soc_simulation(b_soc, sim_range, now, charge_commands, allow_discharge=True, no_solar_to_bat=False, b_min_soc=min_soc, dynamic_floors=d_floors)
+
             # v11.9.497: Synchronize all SOC sources (Tiles + Graph)
             res["soc_simulation"] = {}
             for h in sim_range:
