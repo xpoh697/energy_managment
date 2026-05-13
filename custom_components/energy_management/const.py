176e36c6f1f5d7dee6721e101089ff2f7774a4d6
Vitaly <vitaly@xpoh.ru>
2026-05-13 13:21:42 +0200

    Bump: v11.9.535 - Ready for proper deployment with cache clearing

--- custom_components/energy_management/const.py
+++ custom_components/energy_management/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "energy_management"
-VERSION = "v11.9.527"
-VERSION_CODE = 1109527
+VERSION = "v11.9.535"
+VERSION_CODE = 1109535
 
 CONF_CONSUMPTION_SENSORS = "consumption_sensors"
 CONF_GENERATION_SENSORS = "generation_sensors"
