648270d577a7568fdc27ae82b470a45be78b05ec
Vitaly <vitaly@xpoh.ru>
2026-05-13 13:24:59 +0200

    Bump: v11.9.537 - Fixing simulation log priority in UI (sensor.py)

--- custom_components/energy_management/const.py
+++ custom_components/energy_management/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "energy_management"
-VERSION = "v11.9.535"
-VERSION_CODE = 1109535
+VERSION = "v11.9.537"
+VERSION_CODE = 1109537
 
 CONF_CONSUMPTION_SENSORS = "consumption_sensors"
 CONF_GENERATION_SENSORS = "generation_sensors"
