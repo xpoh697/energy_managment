b6be5e5c302a8bcdd76600a87fe5e340962867fa
Vitaly <vitaly@xpoh.ru>
2026-05-13 13:39:16 +0200

    Bump: v11.9.540 - Ready to update survival floor logic (strategy_buy.py)

--- custom_components/energy_management/const.py
+++ custom_components/energy_management/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "energy_management"
-VERSION = "v11.9.539"
-VERSION_CODE = 1109539
+VERSION = "v11.9.540"
+VERSION_CODE = 1109540
 
 CONF_CONSUMPTION_SENSORS = "consumption_sensors"
 CONF_GENERATION_SENSORS = "generation_sensors"
