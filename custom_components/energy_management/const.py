debc350ff36ca4cf08cdeedc1c24478a00b56f46
Vitaly <vitaly@xpoh.ru>
2026-05-13 13:32:41 +0200

    Bump: v11.9.539 - Ready to inject manual mode power into run_soc_simulation (strategy_base.py)

--- custom_components/energy_management/const.py
+++ custom_components/energy_management/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "energy_management"
-VERSION = "v11.9.538"
-VERSION_CODE = 1109538
+VERSION = "v11.9.539"
+VERSION_CODE = 1109539
 
 CONF_CONSUMPTION_SENSORS = "consumption_sensors"
 CONF_GENERATION_SENSORS = "generation_sensors"
