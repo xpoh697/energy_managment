74118e670576a3e9713a2062e722d3a96da666f2
Vitaly <vitaly@xpoh.ru>
2026-05-15 16:49:05 +0200

    v11.9.735: Synchronized survival logic with main sim_log (removed _log_sun dependency).

--- custom_components/energy_management/const.py
+++ custom_components/energy_management/const.py
@@ -1,7 +1,7 @@
 DOMAIN = "energy_management"
-VERSION = "v11.9.733"
+VERSION = "v11.9.735"
 
-VERSION_CODE = 1109733
+VERSION_CODE = 1109735
 
 CONF_CONSUMPTION_SENSORS = "consumption_sensors"
 CONF_GENERATION_SENSORS = "generation_sensors"
