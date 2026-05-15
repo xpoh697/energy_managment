2507b2366139a82eebecbc0ce4843804afe20e64
Vitaly <vitaly@xpoh.ru>
2026-05-15 17:01:36 +0200

    v11.9.738: Final stable version. Fixed NameError in StrategyBuy. Reverted all unnecessary Sell logic.

--- custom_components/energy_management/const.py
+++ custom_components/energy_management/const.py
@@ -1,7 +1,7 @@
 DOMAIN = "energy_management"
-VERSION = "v11.9.737"
+VERSION = "v11.9.738"
 
-VERSION_CODE = 1109737
+VERSION_CODE = 1109738
 
 CONF_CONSUMPTION_SENSORS = "consumption_sensors"
 CONF_GENERATION_SENSORS = "generation_sensors"
