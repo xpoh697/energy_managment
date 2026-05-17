bd9c676a950037cecb4ad376bf12fe4442e5c90c
Vitaly <vitaly@xpoh.ru>
2026-05-17 20:39:43 +0200

    chore: bump version to v12.1.1

--- custom_components/energy_management/const.py
+++ custom_components/energy_management/const.py
@@ -1,7 +1,7 @@
 DOMAIN = "energy_management"
-VERSION = "v12.1.0"
+VERSION = "v12.1.1"
 
-VERSION_CODE = 1201000
+VERSION_CODE = 1201001
 
 CONF_CONSUMPTION_SENSORS = "consumption_sensors"
 CONF_GENERATION_SENSORS = "generation_sensors"
